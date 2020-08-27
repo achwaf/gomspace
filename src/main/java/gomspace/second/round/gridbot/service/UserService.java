73a744fa0d8002dd4db22cd65b36fb68ea96690c
achra <achra@Xhabloo.fritz.box>
2020-08-27 19:53:16 +0200

    project creation

---
+++ src/main/java/gomspace/second/round/gridbot/service/UserService.java
@@ -0,0 +1,41 @@
+package gomspace.second.round.gridbot.service;
+
+import gomspace.second.round.gridbot.entity.User;
+import gomspace.second.round.gridbot.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
